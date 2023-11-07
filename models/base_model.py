9195a3d8c551786ead9468fc404d630949dc0c57
Iyad M. Mahrous <iyad.m.mahrous@gmail.com>
2023-11-07 02:02:27 -0800

    Task 3

--- models/base_model.py
+++ models/base_model.py
@@ -20,9 +20,23 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         '''Startup function'''
         self.id = str(uuid.uuid4())
-        self.create_at = datetime.datetime.now()
-        self.updated_at = self.create_at
+        self.created_at = datetime.datetime.now()
+        self.updated_at = self.created_at
 
     def __str__(self):
         '''String Presentation'''
         return (f'[{type(self).__name__}] ({self.id}) {str(self.__dict__)}')
+
+    def save(self):
+        '''updates the public instance attribute updated_at with
+        the current datetime'''
+        self.updated_at = datetime.datetime.now()
+
+    def to_dict(self):
+        '''returns a dictionary containing all keys/values
+        of __dict__ of the instance'''
+        new_dict = self.__dict__
+        new_dict['__class__'] = type(self).__name__
+        new_dict['created_at'] = self.created_at.isoformat()
+        new_dict['updated_at'] = self.updated_at.isoformat()
+        return new_dict
