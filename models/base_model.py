a44b77e5a368d202ed4385e8a36ae7298845800e
Iyad M. Mahrous <iyad.m.mahrous@gmail.com>
2023-11-08 01:34:20 -0800

    Task 3

--- models/base_model.py
+++ models/base_model.py
@@ -19,13 +19,24 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         '''Startup function'''
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = self.created_at
+        if len(kwargs) == 0:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.datetime.now()
+            self.updated_at = self.created_at
+        else:
+            del(kwargs['__class__'])
+            kwargs['created_at'] = \
+                datetime.datetime.fromisoformat(kwargs['created_at'])
+            kwargs['updated_at'] = \
+                datetime.datetime.fromisoformat(kwargs['updated_at'])
+            for key, value in kwargs.items():
+                setattr(self, key, value)
 
     def __str__(self):
         '''String Presentation'''
-        return (f'[{type(self).__name__}] ({self.id}) {str(self.__dict__)}')
+        return (
+                f'[{self.__class__.__name__}] ({self.id}) {str(self.__dict__)}'
+               )
 
     def save(self):
         '''updates the public instance attribute updated_at with
@@ -36,7 +47,7 @@ class BaseModel:
         '''returns a dictionary containing all keys/values
         of __dict__ of the instance'''
         new_dict = self.__dict__
-        new_dict['__class__'] = type(self).__name__
+        new_dict['__class__'] = self.__class__.__name__
         new_dict['created_at'] = self.created_at.isoformat()
         new_dict['updated_at'] = self.updated_at.isoformat()
         return new_dict
