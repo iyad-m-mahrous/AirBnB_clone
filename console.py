87c66b2ce89392bd6212ae24ec5bdc46785c7710
Iyad M. Mahrous <iyad.m.mahrous@gmail.com>
2023-11-10 04:56:51 -0800

    Task 9

--- console.py
+++ console.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
 '''contains the entry point of the command interpreter'''
 import cmd
-from models.user import User
 from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 from models import storage
 
 
